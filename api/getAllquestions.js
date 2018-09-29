6da751ea0a1e89c30127d2191b656c81c3d2f2f0
ajimae <meeky.ae@gmail.com>
2018-09-29 04:24:15 +0100

    Add get all questions script

--- api/getAllquestions.js
+++ api/getAllquestions.js
@@ -24,17 +24,7 @@ fetch(url, {
     if(data.success) {
         data.Questions.map((value) => {
             console.log(value);
-            document.getElementById('article').innerHTML = ''+
-                +'<div class="questions">'+
-                    +"<p>"+
-                        +'<a href="view.html">'+value.title+'</a>'+
-                    +'</p>'+
-               +'</div>'+
-                +'<footer>'+
-                        +'<div class="status">Posted by <a href="person.html">'+value.username+'</a>&nbsp;&nbsp;'+
-                        +'<span class="vote">0 votes</span>&nbsp;&nbsp;<span class="ans">'+data.views+' Views</span>&nbsp;&nbsp;'+
-                        +'<span>'+data.posted_at+' ago</span></div>'+
-                +'</footer>';
+            document.getElementById('article').innerHTML = `<div class="questions"><p><a href="view.html">${value.title}</a></p></div><footer><div class="status">Posted by <a href="person.html">${value.username}</a>&nbsp;&nbsp;<span class="vote">0 votes</span>&nbsp;&nbsp;<span class="ans">${value.views} Views</span>&nbsp;&nbsp;<span>4 hrs ago</span></div></footer>`;
         });
     }else if(data.error) {
         console.log(data.error);
--- api/test.js
+++ api/test.js
@@ -1,19 +1,37 @@
-let VOWEL_LETTERS = ['a', 'e', 'i', 'o', 'u'];
+//let VOWEL_LETTERS = ['a', 'e', 'i', 'o', 'u'];
+//
+//const letter = 'O';  //coming from user input
+//
+//
+//if(isVowel(letter)) {
+//    console.log(`${letter} is a vowel`);
+//}
+//else {
+//    console.log(`${letter} is not a vowel`);
+//}
+//
+//function isVowel(n) {
+//    n = n.toLowerCase().trim();
+//    for(let i of VOWEL_LETTERS) {
+//        if(i === n) return true;
+//    }    
+//    return false;
+//}
 
-const letter = 'O';  //coming from user input
+    
+const html = '<div class="questions"><p><a href="view.html"></a></p></div><footer><div class="status">Posted by <a href="person.html">Username</a>&nbsp;&nbsp;<span class="vote">0 votes</span>&nbsp;&nbsp;<span class="ans">5 Views</span>&nbsp;&nbsp;<span>4 hrs ago</span></div></footer>';
 
 
-if(isVowel(letter)) {
-    console.log(`${letter} is a vowel`);
-}
-else {
-    console.log(`${letter} is not a vowel`);
-}
+//const html = '<div class="questions">'+
+//    +'<p>'+
+//        +'<a href="view.html">'+value.title+'</a>'+
+//    +'</p>'+
+//+'</div>'+
+//+'<footer>'+
+//    +'<div class="status">Posted by <a href="person.html">'+value.username+'</a>&nbsp;&nbsp;'+
+//    +'<span class="vote">0 votes</span>&nbsp;&nbsp;<span class="ans">'+data.views+' Views</span>&nbsp;&nbsp;'+
+//    +'<span>'+data.posted_at+' ago</span></div>'+
+//+'</footer>';
 
-function isVowel(n) {
-    n = n.toLowerCase().trim();
-    for(let i of VOWEL_LETTERS) {
-        if(i === n) return true;
-    }    
-    return false;
-}
+
+document.getElementById('article').innerHTML = html;
