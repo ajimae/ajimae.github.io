3a22700c37171ec5a95853291bdf507cb7949158
ajimae <meeky.ae@gmail.com>
2018-09-30 12:28:10 +0100

    Add a postQuestion.js file to post questions

--- api/getAllQuestions.js
+++ api/getAllQuestions.js
@@ -1,15 +1,3 @@
-//const questions = '<div class="questions">'+
-//                    +"<p>"+
-//                        +'<a href="view.html">'+data.question+'</a>'+
-//                    +'</p>'+
-//               +'</div>'+
-//                +'<footer>'+
-//                        +'<div class="status">Posted by <a href="person.html">'+data.username+'</a>&nbsp;&nbsp;'+
-//                        +'<span class="vote">0 votes</span>&nbsp;&nbsp;<span class="ans">0 Answers</span>&nbsp;&nbsp;'+
-//                        +'<span>2hrs ago</span></div>'+
-//                +'</footer>';
-//
-
 const url = 'https://ajimae.herokuapp.com/api/v1/questions';
 fetch(url, {
     method: 'GET',
@@ -17,7 +5,6 @@ fetch(url, {
         'Content-Type': 'application/json; charset=utf-8'
     },
 }).then(response => response.json()).then((data) => {
-    //console.log(data);
     if(data.success) {
         data.Questions.map((value) => {
             let elementDiv = document.createElement('div');
---
+++ api/postQuestion.js
@@ -0,0 +1,81 @@
+/**
+** Created by ajima chukwuemeka
+*/
+
+const titleElement = document.getElementById('title');
+const descriptionElement = document.getElementById('modify');
+const postButtonElement = document.getElementById('post');
+const successElement = document.getElementById('success');
+let errorCount = 0;
+
+const checkOthers = (value, element) => {
+    if(value === '' || !value.replace(/\s/g, '').length) {
+        errorCount++;
+        element.style.border = '1px solid #e64a4a';
+    }else {
+        element.style.border = '1px solid #06b506';
+    }
+}
+
+const postQuestion = (details) => {
+    const url = 'https://ajimae.herokuapp.com/api/v1/questions';
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+            'x-access-token': token
+        },
+        body: JSON.stringify(details)
+    })
+    .then(response => response.json()).then((data) => {
+        if(data.success) {
+            postButtonElement.removeAttribute('disabled');
+            successElement.style.display = 'block';
+            successElement.style.color = '#222';
+            successElement.style.fontSize = '15px';
+            successElement.innerHTML = 'Question was posted successful....Redirecting to activity page';
+            setTimeout(() => {
+                successElement.innerHTML = 'Redirecting...';
+                window.location.href = window.location.protocol + '//' + window.location.hostname + '/activity.html';
+            }, 3000);
+        }else if(data.error) {
+            postButtonElement.removeAttribute('disabled');
+            postButtonElement.value = 'Sign in';
+            successElement.style.display = 'block';
+            
+            successElement.style.color = '#222';
+            successElement.style.fontSize = '15px';
+            successElement.innerHTML = data.error;
+            console.log(data.error);
+        }
+    }).catch((error) => {
+        console.log('Request failed', error);
+    });
+}
+
+const checkInput = () => {
+    errorCount = 0;
+    const title = titleElement.value;
+    const description = descriptionElement.value;
+    
+    checkOthers(title, titleElement);
+    checkOthers(description, descriptionElement);
+    
+    if(errorCount > 0) {
+        successElement.style.color = 'red';
+        successElement.style.fontSize = '15px';
+        successElement.innerHTML = 'The field(s) with red borders are either empty or invalid.';
+        return false;
+    }else {
+        successElement.style.display = 'none';
+    }
+    
+    postButtonElement.setAttribute('disabled', '');
+    postButtonElement.value = 'Posting question...';
+    
+    const details = {
+        title,
+        description
+    }
+    postQuestion(details);
+}
