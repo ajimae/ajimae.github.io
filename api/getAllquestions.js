fcffbc6ee356513aae0857f587999af22a9b3d5e
ajimae <meeky.ae@gmail.com>
2018-09-30 08:40:24 +0100

    Modified the getAllquestions script

--- api/getAllquestions.js
+++ api/getAllquestions.js
@@ -20,9 +20,12 @@ fetch(url, {
         'Content-Type': 'application/json; charset=utf-8'
     },
 }).then(response => response.json()).then((data) => {
+    //console.log(data);
     if(data.success) {
-        document.getElementById('article').innerHTML = data.Questions.map((value) => {
-             `<div class="questions"><p><a href="view.html">${value.title}</a></p></div><footer><div class="status">Posted by <a href="person.html">${value.username}</a>&nbsp;&nbsp;<span class="vote">0 votes</span>&nbsp;&nbsp;<span class="ans">${value.views} Views</span>&nbsp;&nbsp;<span>4 hrs ago</span></div></footer>`;
+        data.Questions.map((value) => {
+            let item = `<div class="article" id="article"><div class="questions"><p><a href="view.html">${value.title}</a></p></div><footer><div class="status">Posted by <a href="person.html">${value.username}</a>&nbsp;&nbsp;<span class="vote">0 votes</span>&nbsp;&nbsp;<span class="ans">${value.views} Views</span>&nbsp;&nbsp;<span>4 hrs ago</span></div></footer></div>`;
+            
+            document.getElementById('content').appendChild(item);
         });
     }else if(data.error) {
         console.log(data.error);
