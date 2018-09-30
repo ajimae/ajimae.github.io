d08f84969f30bdf674e13cb02128a322d755613b
ajimae <meeky.ae@gmail.com>
2018-09-30 15:59:59 +0100

    Add a postQuestion.js file to post question

--- api/postQuestion.js
+++ api/postQuestion.js
@@ -19,6 +19,7 @@ const checkOthers = (value, element) => {
 
 const postQuestion = (details) => {
     const url = 'https://ajimae.herokuapp.com/api/v1/questions';
+    console.log(localStorage.getItem('x-access-token'));
     fetch(url, {
         method: 'POST',
         headers: {
