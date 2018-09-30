056d439f956e9eb5857aaacadff3a55aff362427
ajimae <meeky.ae@gmail.com>
2018-09-30 08:46:50 +0100

    Modified the getAllquestions script

--- api/getAllquestions.js
+++ api/getAllquestions.js
@@ -11,9 +11,6 @@
 //
 
 const url = 'https://ajimae.herokuapp.com/api/v1/questions';
-//const questions = `<div class="questions"><p><a href="view.html">${value}</a></p></div>`;
-
-
 fetch(url, {
     method: 'GET',
     headers: {
@@ -25,6 +22,7 @@ fetch(url, {
         data.Questions.map((value) => {
             let item = `<div class="article" id="article"><div class="questions"><p><a href="view.html">${value.title}</a></p></div><footer><div class="status">Posted by <a href="person.html">${value.username}</a>&nbsp;&nbsp;<span class="vote">0 votes</span>&nbsp;&nbsp;<span class="ans">${value.views} Views</span>&nbsp;&nbsp;<span>4 hrs ago</span></div></footer></div>`;
             
+            console.log(item);
             document.getElementById('content').appendChild(item);
         });
     }else if(data.error) {
